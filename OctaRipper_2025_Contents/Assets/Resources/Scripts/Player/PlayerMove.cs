54dc314b6a04d522e634e042f37d7c4bf19133c6
MDAIKI1230 <shimamizu1230@gmail.com>
2025-10-01 17:58:07 +0900

    プレイヤーの移動の実装
    プレイヤーの移動と傾きを追加しました

--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Player/PlayerMove.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Player/PlayerMove.cs
@@ -6,11 +6,18 @@ public class PlayerMove : MonoBehaviour
     InputSystem_Actions input;
     Rigidbody rb;
 
-    Vector3 moveVec = Vector3.zero; //入力からの移動方向を入れる変数
+    Vector3 moveVec = Vector3.zero;   //入力からの移動方向を入れる変数
+    Quaternion moveRot; //入力からプレイヤーの傾きを入れる変数
 
-    [Header("プレイヤーの移動")]
+    [Header("プレイヤーの移動のスピード")]
     [SerializeField]
     private float speed;
+    [Header("プレイヤーの移動時の傾くスピード")]
+    [SerializeField]
+    private float angleSpeed;
+    [Header("プレイヤーの移動時の傾き")]
+    [SerializeField]
+    private float angle;
     private void OnEnable()
     {
         input = new InputSystem_Actions();
@@ -35,13 +42,20 @@ public class PlayerMove : MonoBehaviour
     void FixedUpdate()
     {
         rb.linearVelocity = moveVec;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, moveRot, Time.deltaTime * angleSpeed);
     }
 
+    //移動用の関数
     private void Move(InputAction.CallbackContext _context)
     {
         Debug.Log(_context.ReadValue<Vector2>());
         Vector2 input = _context.ReadValue<Vector2>();
+        //移動のベクトル生成
         Vector3 moveDir = new Vector3(input.x, 0, input.y);
         moveVec = moveDir * speed;
+
+        //傾きの生成
+        Vector3 moveAngle = new Vector3(moveDir.z, 0, -moveDir.x) * angle;
+        moveRot = Quaternion.Euler(moveAngle);
     }
 }
