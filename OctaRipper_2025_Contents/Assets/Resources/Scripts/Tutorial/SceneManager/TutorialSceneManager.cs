049186073d976701fffcaa8102c4f2156a3171f6
kazu06121 <ymkazu1@gmail.com>
2025-10-01 16:44:04 +0900

    シーン遷移
    シーンをリストに追加し、シーンマネージャのオブジェクトにスクリプトをアタッチして動作確認

--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Tutorial/SceneManager/TutorialSceneManager.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Tutorial/SceneManager/TutorialSceneManager.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TutorialSceneManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Title");
+        }
     }
 }
