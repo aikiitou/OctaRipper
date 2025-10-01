11e41a305ee2a235b8c7aecd4719db87b5f98dc5
MDAIKI1230 <shimamizu1230@gmail.com>
2025-10-01 17:33:37 +0900

    プレイヤーの移動
    プレイヤーの移動を簡単に実装

--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Player/PlayerMove.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Player/PlayerMove.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
-{ 
+{
+    InputSystem_Actions input;
+    Rigidbody rb;
+
+    Vector3 moveVec = Vector3.zero; //入力からの移動方向を入れる変数
+
+    [Header("プレイヤーの移動")]
+    [SerializeField]
+    private float speed;
+    private void OnEnable()
+    {
+        input = new InputSystem_Actions();
+        input.Enable();
+        //インプットシステムに関数の追加
+        input.Player.Move.performed += Move;
+        input.Player.Move.canceled += Move;
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+        //インプットシステムに関数の解除
+        input.Player.Move.performed -= Move;
+        input.Player.Move.canceled -= Move;
+    }
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.linearVelocity = moveVec;
+    }
+
+    private void Move(InputAction.CallbackContext _context)
+    {
+        Debug.Log(_context.ReadValue<Vector2>());
+        Vector2 input = _context.ReadValue<Vector2>();
+        Vector3 moveDir = new Vector3(input.x, 0, input.y);
+        moveVec = moveDir * speed;
     }
 }
