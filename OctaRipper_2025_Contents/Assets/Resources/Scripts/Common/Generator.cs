dd4a5a0f43bd3a59ab16c4c036b4fd72f8fee6d3
itou aiki <itouaiki46@gmail.com>
2025-10-02 09:56:53 +0900

    Generatorの動作チェックとそのためのテスト用オブジェクト等の追加
    ・Test用のマネージャースクリプトの作成
    ・Test用のPrefab3種類の作成
    ・GeneratorのPrefab化

--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Common/Generator.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Common/Generator.cs
@@ -1,9 +1,9 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-
     [System.Serializable]
     private struct GenerateObject
     {
@@ -12,18 +12,30 @@ public class Generator : MonoBehaviour
     }
 
     [SerializeField]
-    private List<GenerateObject> sGenerateObjects;
-    
+    private GenerateObject sGenerateObject;
+
+    private List<GameObject> gObjectPools = new List<GameObject>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Generate();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Generate()
     {
-        
+        if(sGenerateObject.generateObject == null)
+        {
+            return;
+        }
+
+        for(int i = 0; i < sGenerateObject.generateNum; ++i)
+        {
+            GameObject obj = Instantiate(sGenerateObject.generateObject);
+            obj.transform.parent = this.transform;
+            obj.SetActive(false);
+            gObjectPools.Add(obj);
+        }
+
     }
 }
