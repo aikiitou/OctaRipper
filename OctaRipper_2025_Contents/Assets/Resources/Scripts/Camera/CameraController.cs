4c6b913183e6882f76f2518a904ea338ef99d287
MDAIKI1230 <shimamizu1230@gmail.com>
2025-10-02 11:14:40 +0900

    X方向の視点移動
    X方向の視点移動を実装しました

---
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Camera/CameraController.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    InputSystem_Actions input;
+
+    [Header("カメラを置きたい位置にオブジェクトをおいて入れる")]
+    [SerializeField]
+    Transform targetPos;
+    [Header("プレイヤー")]
+    [SerializeField]
+    Transform player;
+    [Header("カメラの移動,回転スピード")]
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float limitAngle;
+
+    [SerializeField]
+    float sens;
+
+    private Vector3 lookOffset = new Vector3(0, 1f, 0);
+    private void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        input = new InputSystem_Actions();
+        input.Enable();
+
+        //インプットシステムに関数登録
+        input.Player.Look.performed += ViewPointMovement;
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+
+        //インプットシステムの解除
+        input.Player.Look.performed -= ViewPointMovement; 
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos.position, Time.deltaTime * speed);
+
+        transform.LookAt(player.position + lookOffset);
+    }
+
+    private void ViewPointMovement(InputAction.CallbackContext _context)
+    {
+        //Vector2 input = _context.ReadValue<Vector2>();
+        //float angleY = Mathf.Clamp(input.y, -limitAngle, limitAngle);
+
+        //transform.Translate(new Vector3(0, Mathf.Sin(angleY), 0));
+    }
+}
--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Player/PlayerMoveController.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Player/PlayerMoveController.cs
@@ -18,22 +18,27 @@ public class PlayerMoveController : MonoBehaviour
     [Header("プレイヤーの移動時の傾き")]
     [SerializeField]
     private float angle;
+    [Header("マウスセンシ")]
+    [SerializeField]
+    private float sens;
     private void OnEnable()
     {
         input = new InputSystem_Actions();
         input.Enable();
         //インプットシステムに関数の追加
         input.Player.Move.performed += Move;
-        input.Player.Move.canceled += Move;
+        input.Player.Move.canceled += Stop;
         input.Player.Dodge.started += Dodge;
+        input.Player.Look.performed += ViewPointMovementX;
     }
     private void OnDisable()
     {
         input.Disable();
         //インプットシステムに関数の解除
         input.Player.Move.performed -= Move;
-        input.Player.Move.canceled -= Move;
-        input.Player.Dodge.started += Dodge;
+        input.Player.Move.canceled -= Stop;
+        input.Player.Dodge.started -= Dodge;
+        input.Player.Look.performed -= ViewPointMovementX;
     }
     void Start()
     {
@@ -43,25 +48,45 @@ public class PlayerMoveController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = moveVec;
+        rb.AddForce(moveVec);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, moveRot, Time.deltaTime * angleSpeed);
     }
 
     //移動用の関数
     private void Move(InputAction.CallbackContext _context)
     {
-        Debug.Log(_context.ReadValue<Vector2>());
+        rb.linearVelocity = Vector3.zero;
         Vector2 input = _context.ReadValue<Vector2>();
         //移動のベクトル生成
         Vector3 moveDir = new Vector3(input.x, 0, input.y);
         moveVec = moveDir * speed;
 
         //傾きの生成
-        Vector3 moveAngle = new Vector3(moveDir.z, 0, -moveDir.x) * angle;
+        Vector3 moveAngle = new Vector3(moveDir.z * angle, transform.eulerAngles.y, -moveDir.x * angle);
+
         moveRot = Quaternion.Euler(moveAngle);
     }
+    //移動を止める
+    private void Stop(InputAction.CallbackContext _context)
+    {
+        //移動用の情報のリセット
+        moveVec = Vector3.zero;
+        rb.linearVelocity = Vector3.zero;
+        moveRot = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+    }
+    //回避用の関数
     private void Dodge(InputAction.CallbackContext _context)
     {
         transform.localPosition += (moveVec / speed);
+    }
+    //移動方向を傾ける関数
+    private void ViewPointMovementX(InputAction.CallbackContext _context)
+    {
+        //マウスの入力によって回転させる
+        Vector2 input = _context.ReadValue<Vector2>();
+        transform.Rotate(0, input.x * sens, 0);
+
+        //傾き方向を保管
+        moveRot = transform.rotation;
     }
 }
