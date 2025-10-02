a520a3783c16d35002f277b0418250790401e69d
kazu06121 <ymkazu1@gmail.com>
2025-10-02 09:12:44 +0900

    スクリプト名の変更
    チュートリアルのシーンマネージャーをマネージャーに変更し、シーン全体のマネージャーに変更

--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Tutorial/SceneManager/TutorialManager.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Tutorial/SceneManager/TutorialManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TutorialSceneManager : MonoBehaviour
+public class TutorialManager : MonoBehaviour
 {
     void Update()
     {
