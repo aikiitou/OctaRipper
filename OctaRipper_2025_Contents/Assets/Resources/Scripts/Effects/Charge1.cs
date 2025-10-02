6def98859ff8d59d8334ced17390afdbaf53c486
kazu06121 <ymkazu1@gmail.com>
2025-10-02 18:04:25 +0900

    新しいマテリアルの作成とエフェクトの移動
    斬撃エフェクトのマテリアルのテストと溜め攻撃の飛ぶ斬撃のてすと

--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Effects/Charge1.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Effects/Charge1.cs
@@ -2,13 +2,10 @@ using UnityEngine;
 
 public class Charge1 : MonoBehaviour
 {
+    Rigidbody rb;
     void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.linearVelocity = new Vector3(4f, 0f, 0f);
     }
 }
