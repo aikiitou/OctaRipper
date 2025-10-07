2ae1bce6e2874e422931dd3f585dc8776287cf7a
MotokiHashino <hashinomotoki@gmail.com>
2025-10-07 15:11:31 +0900

    Enemy03のシールド判定作成、アニメーション作成開始。
    Enemy03のシールド判定作成、アニメーション作成開始。

--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Enemies/EnemyController02.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Enemies/EnemyController02.cs
@@ -96,10 +96,6 @@ public class EnemyController02 : EnemyControllerBase
         {
             Death(); // Ž€–S
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Damage(-10.0f, (gameObject.transform.position - gTargetObject.transform.position).normalized * 5.0f, 1.0f);
-        }
     }
 
     void ChangePattern()
