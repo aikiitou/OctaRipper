ec6ac84e8264c171f1a63b157e1144eeef0d8873
MotokiHashino <hashinomotoki@gmail.com>
2025-10-09 23:11:20 +0900

    ミサイル実装。
    ミサイル実装。

--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Enemies/BossAttack/MissileController.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Enemies/BossAttack/MissileController.cs
@@ -17,29 +17,49 @@ public class MissileController : MonoBehaviour
     [SerializeField, Header("最大生存時間")]
     float fAliveTime = 5.0f;
 
+
+    [SerializeField, Header("爆発与硬直時間")]
+    float fExplosionFriezeTime = 0.3f;
+
+    [SerializeField, Header("爆発吹っ飛ばし")]
+    float fExplosionForce = 10.0f;
+
+    [SerializeField, Header("爆発オブジェクト")]
+    GameObject gExplosion; // 攻撃の当たり判定オブジェクト
+
+
+    float fCanRotationTimer;
     GameObject gParentObject;
     Rigidbody rRigidBody;
     GameObject gTargetObject; // 対象のオブジェクト
     public void SetUp(GameObject _shotObject, GameObject _parent, Vector3 _pos)
     {
+        fCanRotationTimer = fCanRotationTime;
         gTargetObject = GameObject.FindGameObjectWithTag("Player"); // 対象を代入
         gParentObject = _parent;
         rRigidBody = GetComponent<Rigidbody>();
-        rRigidBody.linearVelocity = _shotObject.transform.up * fSpeed;
-        transform.rotation = Quaternion.FromToRotation(transform.position, _shotObject.transform.position);
+        rRigidBody.linearVelocity = -_shotObject.transform.up * fSpeed;
         transform.parent = null;
         transform.position = _pos;
         gameObject.SetActive(true);
+        transform.rotation = Quaternion.FromToRotation(Vector3.forward, rRigidBody.linearVelocity);
     }
     void Update()
     {
-        rRigidBody.linearVelocity = transform.forward * fSpeed;
-        Vector3 distance = gTargetObject.transform.position - gameObject.transform.position;
-        transform.rotation = Quaternion.Slerp(
-            transform.rotation,
-            Quaternion.FromToRotation(Vector3.forward, distance.normalized),
-            fRotationSpeed * Time.deltaTime
-            ); // 方向転換
+        if (fCanRotationTimer >= 0)
+        {
+            fCanRotationTimer -= Time.deltaTime;
+        }
+        else
+        {
+            rRigidBody.linearVelocity = transform.forward * fSpeed;
+            Vector3 distance = gTargetObject.transform.position - gameObject.transform.position;
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.FromToRotation(Vector3.forward, distance.normalized),
+                fRotationSpeed * Time.deltaTime
+                ); // 方向転換
+        }
     }
 
     public void Release()
@@ -50,6 +70,15 @@ public class MissileController : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != 11)
+        {
+            gExplosion.GetComponent<EnemyExplosionController>().SetUp(gameObject, fExplosionForce, fDamage, fExplosionFriezeTime);
+            MyDebugLib.MessageLog(other.gameObject.layer);
+            Release();
+        }
+    }
 
 
 }
