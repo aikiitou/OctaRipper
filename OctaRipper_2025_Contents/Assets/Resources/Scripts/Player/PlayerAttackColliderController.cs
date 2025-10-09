7c0861e26f6529f71fe8056cbd6f8c1440ed7285
MDAIKI1230 <shimamizu1230@gmail.com>
2025-10-09 13:44:58 +0900

    ヒットストップ仮実装
    プレイヤーの攻撃にヒットストップの仮実装をしました

--- OctaRipper_2025_Contents/Assets/Resources/Scripts/Player/PlayerAttackColliderController.cs
+++ OctaRipper_2025_Contents/Assets/Resources/Scripts/Player/PlayerAttackColliderController.cs
@@ -1,14 +1,18 @@
+using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAttackColliderController : MonoBehaviour
 {
+    const float HIT_STOP_TIME = 0.1f;
+
     List<GameObject> gDamagedObjects = new List<GameObject>();
     private Transform tPlayer;
     private float fDamage = 0f;
     private float fFreezeTime = 0f;
     private float fKnockBackPower = 0f;
+    private bool bIsHitStop = false;
 
     private void Awake()
     {
@@ -28,12 +32,27 @@ public class PlayerAttackColliderController : MonoBehaviour
                 float distance = (enemyPos - playerPos).magnitude;
                 forceVec *= Mathf.Lerp(1, fKnockBackPower, distance);
                 enemy.Damage(-fDamage, forceVec, fFreezeTime);
+                if(bIsHitStop == false)
+                {
+                    HitStop();
+                }
             }
         }
         if(_other.TryGetComponent<FireWallController>(out FireWallController fireWall))
         {
             fireWall.TakeDamage(-fDamage);
+            if (bIsHitStop == false)
+            {
+                HitStop();
+            }
         }
+    }
+    private IEnumerator HitStop()
+    {
+        bIsHitStop = true;
+        Time.timeScale = 0.0f;
+        yield return new WaitForSecondsRealtime(HIT_STOP_TIME);
+        Time.timeScale = 1.0f;
     }
     public void SetAttackInfo(float _damage,float _freezeTime,float _knockBackPower)
     {
@@ -47,5 +66,6 @@ public class PlayerAttackColliderController : MonoBehaviour
         fDamage = 0f;
         fFreezeTime = 0f;
         fKnockBackPower = 0f;
+        bIsHitStop = false;
     }
 }
